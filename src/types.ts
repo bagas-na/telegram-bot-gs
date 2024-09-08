8dbd83d11d3e18f8d7a3772f1377b687b08fc073
bagas-na <bagasnurseno@gmail.com>
2024-09-08 21:38:55 +0700

    change .d.ts emit behavior for JSDoc generation

--- src/types.ts
+++ src/types.ts
@@ -1,23 +1,18 @@
-import { ForceReply, InlineKeyboardMarkup, ReplyKeyboardMarkup, ReplyKeyboardRemove } from "@grammyjs/types";
+import {
+  ForceReply,
+  InlineKeyboardMarkup,
+  ReplyKeyboardMarkup,
+  ReplyKeyboardRemove,
+} from "@grammyjs/types";
 
-export const CATEGORIES = ["RSUD", "PUSKESMAS", "SEKOLAH NEGERI", "DINAS", "SETDA", "BAPENDA", "POLDA"] as const;
-
-export const PROPERTIES = [
-  "submit_proposal",
-  "connectivity",
-  "eazy",
-  "oca",
-  "digiclinic",
-  "pijar",
-  "sprinthink",
-  "nilai_project",
-] as const;
-
-export const FUNNEL_PROPERTIES = ["connectivity", "eazy", "oca", "digiclinic", "pijar", "sprinthink"];
-
+export type ReplyMarkup =
+  | InlineKeyboardMarkup
+  | ReplyKeyboardMarkup
+  | ReplyKeyboardRemove
+  | ForceReply;
 export type CustomerCategory = (typeof CATEGORIES)[number];
 export type CustomerProperty = (typeof PROPERTIES)[number];
-export type ReplyMarkup = InlineKeyboardMarkup | ReplyKeyboardMarkup | ReplyKeyboardRemove | ForceReply;
+export type Funnel = "F0" | "F3" | "F4" | "F5";
 export type UserCache = {
   userState:
     | "select_category"
@@ -30,8 +25,6 @@ export type UserCache = {
   customer_name?: string | null; // untuk menyimpan nama pelanggan di select_property dan update_property
   customer_property?: CustomerProperty | null;
 };
-
-export type Funnel = "F0" | "F3" | "F4" | "F5";
 export type CustomerData = {
   customer_category: string;
   name: string;
@@ -44,7 +37,6 @@ export type CustomerData = {
   sprinthink: Funnel;
   nilai_project: number;
 };
-
 export interface MyPostData extends GoogleAppsScript.Events.AppsScriptHttpRequestEventPostData {
   getDataAsString(): string;
 }
@@ -52,6 +44,33 @@ export interface DoPostEvent extends GoogleAppsScript.Events.DoPost {
   postData: MyPostData;
 }
 
+export const CATEGORIES = [
+  "RSUD",
+  "PUSKESMAS",
+  "SEKOLAH NEGERI",
+  "DINAS",
+  "SETDA",
+  "BAPENDA",
+  "POLDA",
+] as const;
+export const PROPERTIES = [
+  "submit_proposal",
+  "connectivity",
+  "eazy",
+  "oca",
+  "digiclinic",
+  "pijar",
+  "sprinthink",
+  "nilai_project",
+] as const;
+export const FUNNEL_PROPERTIES = [
+  "connectivity",
+  "eazy",
+  "oca",
+  "digiclinic",
+  "pijar",
+  "sprinthink",
+];
 export const CATEGORY_LIST: CustomerCategory[][] = [
   ["RSUD"],
   ["PUSKESMAS"],
@@ -72,7 +91,6 @@ export const PROPERTIES_LIST: CustomerProperty[][] = [
   ["sprinthink"],
   ["nilai_project"],
 ] as const;
-
 export const MAP_PROPS_TO_COL: { [key in CustomerProperty]: number } = {
   submit_proposal: 6,
   connectivity: 7,
@@ -83,7 +101,6 @@ export const MAP_PROPS_TO_COL: { [key in CustomerProperty]: number } = {
   sprinthink: 12,
   nilai_project: 13,
 } as const;
-
 export const MAP_PROPS_TO_TEXT: { [key in CustomerProperty]: string } = {
   submit_proposal: "Submit Proposal",
   connectivity: "Connectivity",
@@ -94,7 +111,6 @@ export const MAP_PROPS_TO_TEXT: { [key in CustomerProperty]: string } = {
   sprinthink: "Sprinthink",
   nilai_project: "Nilai proyek",
 } as const;
-
 export const MAP_COL_TO_PROPS: { [key: number]: CustomerProperty } = {
   "6": "submit_proposal",
   "7": "connectivity",
