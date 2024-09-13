0533be9f1fcfbef7fb889c859262eb95792314ee
bagas-na <105133641+bagas-na@users.noreply.github.com>
2024-09-13 14:49:55 +0700

    add styling option to sent messages

--- src/data/telegramApi.ts
+++ src/data/telegramApi.ts
@@ -1,22 +1,38 @@
-import { Message } from "@grammyjs/types";
 import { ReplyMarkup } from "../types";
+
+type MessageBody = {
+	chat_id: number;
+	text: string;
+	parse_mode?: "MarkdownV2" | "HTML" | "Markdown";
+	reply_markup?: ReplyMarkup;
+}
 
 // Fungsi untuk mengirim teks ke chat dengan markup keyboard
 export async function sendMessage(
 	env: Env,
 	chatId: number,
 	text: string,
+	parse_mode?: "MarkdownV2" | "HTML" | "Markdown",
 	replyMarkup?: ReplyMarkup
 ): Promise<Response> {
 	const apiMethod = "/sendMessage";
 	const Url = env.TELEGRAM_API_URL + env.BOT_TOKEN + apiMethod;
 
 	
-	const messageBody = {
+	const messageBody: MessageBody = {
 		chat_id: chatId,
 		text,
-		reply_markup: replyMarkup,
 	};
+
+	if (parse_mode) {
+		messageBody.parse_mode = parse_mode
+	}
+
+	if (replyMarkup) {
+		messageBody.reply_markup = replyMarkup
+	}
+
+
 	console.log(`Sending message to ${Url}, ${JSON.stringify(messageBody)}`)
 
 	const options: RequestInit = {
